2963918943408e321e52e1cd227b3fd95ecff0e7
KinyWolf <elkkun06@gmail.com>
2024-01-11 07:58:07 +0200

    W

--- movement.gd
+++ movement.gd
@@ -9,12 +9,13 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 var ISFACINGRIGHT = true
 
-
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
+	
+	
+	
 	# Handle jump.
 	if Input.is_action_just_pressed("move_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
@@ -28,12 +29,23 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		
 	# Rotate player sprite to direction you're moving
-	flip()
+	if direction != 0:
+		$Sprite2D.flip_h = (direction == -1)
+	
+	animate(direction)
 
 	move_and_slide()
-	
-func flip():
-	if ISFACINGRIGHT && velocity.x < 0.0 || !ISFACINGRIGHT && velocity.x > 0.0:
-		ISFACINGRIGHT = !ISFACINGRIGHT
-		%player.scale.x *= -1
 		
+func animate(direction):
+	if is_on_floor():
+		if direction == 0:
+			$AnimationPlayer.play("idle")
+		else:
+			$AnimationPlayer.play("run")
+	else :
+		if velocity.y < -100:
+			$AnimationPlayer.play("jump")
+		elif velocity.y > 100:
+			$AnimationPlayer.play("fall")
+		else:
+			$AnimationPlayer.play("levitate")
