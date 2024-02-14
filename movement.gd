8831f98c7d237cc05495ff8f3620b7c7ce4966d6
KinyWolf <elkkun06@gmail.com>
2024-02-14 09:08:45 +0200

    aa

--- movement.gd
+++ movement.gd
@@ -4,31 +4,31 @@ extends CharacterBody2D
 const SPEED = 200.0
 const JUMP_VELOCITY = -400.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 var ISFACINGRIGHT = true
 
+var ISCROUCHING
+
 func _physics_process(delta):
-	# Add the gravity.
+
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	
-	
-	
-	# Handle jump.
 	if Input.is_action_just_pressed("move_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
+	if Input.is_action_pressed("move_crouch") and is_on_floor():
+		ISCROUCHING = true
+	else:
+		ISCROUCHING = false
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		
-	# Rotate player sprite to direction you're moving
 	if direction != 0:
 		$Sprite2D.flip_h = (direction == -1)
 	
@@ -37,15 +37,21 @@ func _physics_process(delta):
 	move_and_slide()
 		
 func animate(direction):
-	if is_on_floor():
-		if direction == 0:
-			$AnimationPlayer.play("idle")
+	if ISCROUCHING:
+		if direction != 0:
+			$AnimationPlayer.play("crouch_walk")
 		else:
-			$AnimationPlayer.play("run")
-	else :
-		if velocity.y < -100:
-			$AnimationPlayer.play("jump")
-		elif velocity.y > 100:
-			$AnimationPlayer.play("fall")
+			$AnimationPlayer.play("crouch_idle")
+	else:
+		if is_on_floor():
+			if direction == 0:
+				$AnimationPlayer.play("idle")
+			else:
+				$AnimationPlayer.play("run")
 		else:
-			$AnimationPlayer.play("levitate")
+			if velocity.y < -100:
+				$AnimationPlayer.play("jump")
+			elif velocity.y > 100:
+				$AnimationPlayer.play("fall")
+			else:
+				$AnimationPlayer.play("levitate")
